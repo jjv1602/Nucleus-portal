51c662818e95bc82b81b5f883f1cdebadefb0465
Joel <joel.varghese2020@vitbhopal.ac.in>
2022-08-19 10:35:48 +0530

    commit3

--- Backend/Controllers/userControllers.js
+++ Backend/Controllers/userControllers.js
@@ -1,12 +1,42 @@
-const registerUser=async(req,res)=>{
+const asyncHandler = require('express-async-handler')
+const User = require("../models/userModels")
+
+const registerUser = async (req, res) => {
 
     // taking name,email and pwd from user
-    const {name,email,password}=req.body;
+    const { name, email, password } = req.body;
 
-    // Returning name and email to user
-    res.json({
+    const userExists = await User.findOne({ email });
+    // If user exists then show error
+    if (userExists) {
+        res.status(404);
+        throw new Error("User already exists");
+    }
+
+    // else if user does not exist then create a new for this we use .create function
+    const user = await User.create({
         name,
         email,
+        password,
     });
+
+    // if user is successfully created that is the input follows the schema then this condition ->if(user)
+    if (user) {
+        // send response in json file
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            // token: generateToken(user._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error("User not found");
+    }
+    // // Returning name and email to user
+    // res.json({
+    //     name,
+    //     email,
+    // });
 };
-module.exports={registerUser}
+module.exports = { registerUser }
