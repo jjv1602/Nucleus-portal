b0a0e1d6a31105d2b12d3515119952b8fc901c8b
Joel <joel.varghese2020@vitbhopal.ac.in>
2022-12-04 18:23:29 +0530

    Revert "deploymentfile"
    This reverts commit b04b0a28198d322810fed590001e109022349303.

--- Backend/server.js
+++ Backend/server.js
@@ -20,12 +20,20 @@ app.use('/api/events',eventRoutes);   //eventRoutes is imported above
 //     res.send("API is running");
 // })
 
+const { errorHandler, notFound } = require('./middlewares/errorMiddlewares');
+app.use(notFound)
+app.use(errorHandler)
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,console.log(`Server started on port ${PORT} `));
+
+
 // --------------------------deployment------------------------------
 
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
@@ -36,9 +44,3 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // --------------------------deployment------------------------------
-const { errorHandler, notFound } = require('./middlewares/errorMiddlewares');
-app.use(notFound)
-app.use(errorHandler)
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,console.log(`Server started on port ${PORT} `));
-
