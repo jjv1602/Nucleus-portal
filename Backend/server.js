e38a6b38564a5895b6749b341fa95d6be9092b96
Joel <joel.varghese2020@vitbhopal.ac.in>
2022-12-05 20:25:12 +0530

    Revert "d3cyc"
    This reverts commit 301cac64c4acec3999042be5a374cb55b4692bae.

--- Backend/server.js
+++ Backend/server.js
@@ -24,12 +24,17 @@ app.use('/api/events',eventRoutes);   //eventRoutes is imported above
 
 const __dirname1 = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "./../frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))  // go inside frontend then build then inside index.html
   );
-
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 // --------------------------deployment------------------------------
 
