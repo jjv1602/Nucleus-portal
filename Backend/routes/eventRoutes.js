7ea5ca6bf106e7b9217a8034cedd8a158a575027
Joel <joel.varghese2020@vitbhopal.ac.in>
2023-02-02 11:45:57 +0530

    REMOVED PROTECT

--- Backend/routes/eventRoutes.js
+++ Backend/routes/eventRoutes.js
@@ -7,14 +7,14 @@ const router=express.Router()
 router.route('/').get( getEvents);  //protect is middleware
 //here on get request it would send getEvents res.json file 
 
-router.route('/get').get(protect,getEventscreatedbyparticularperson);
+router.route('/get').get(getEventscreatedbyparticularperson);
 // to create a event
-router.route('/create').post(protect, createEvent);
+router.route('/create').post(createEvent);
 
 // suppose we get a route of id so we should be able to get the the event , edit (put) the event and delete the event so we add .get() .put and .delete()
-router.route('/:id').get(getEventById).put(protect,updateEvent).delete(deleteEvent);
+router.route('/:id').get(getEventById).put(updateEvent).delete(deleteEvent);
 
-router.route('/:id/rsvp').put(protect,rsvp);
+router.route('/:id/rsvp').put(rsvp);
 router.route('/:id/remove_rsvp').put(remove_rsvp);
 router.route('/:id/remove_rsvp_list_event').put(remove_rsvp_from_table);
 
